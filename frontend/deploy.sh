a1ffce46ee8b0de95039dd22c496abb9dbc223b1
Kim Borgen <kimatborgen@gmail.com>
2017-02-08 16:34:33 +0100

    update deploy and install scripts again

--- frontend/deploy.sh
+++ frontend/deploy.sh
@@ -1,4 +1,4 @@
-git pull
+git pull &&
 npm install &&
 npm run build &&
 cp -r ~/stelios/frontend/build/* /var/www/kimborgen.com/html/
--- install.sh
+++ install.sh
@@ -1,19 +1,20 @@
 
 
-curl -sL https://deb.nodesource.com/setup_7.x | sudo -E bash -
-apt-get install -y build-essential
-apt-get install -y nodejs
+curl -sL https://deb.nodesource.com/setup_7.x | sudo -E bash - &&
+apt-get install -y build-essential &&
+apt-get install -y nodejs &&
 
+apt-get install dos2unix &&
 
-npm install -g create-react-app
-npm install -g react-scripts
+npm install -g create-react-app &&
+npm install -g react-scripts &&
 
-apt-get install libpq-dev
-apt install python3-pip
-pip3 install --upgrade pip
+apt-get install libpq-dev &&
+apt install python3-pip &&
+pip3 install --upgrade pip &&
 
-pip3 install django
-pip3 install djangorestframework
-pip3 install markdown
-pip3 install django-filter
-pip3 install psycopg2
+pip3 install django &&
+pip3 install djangorestframework &&
+pip3 install markdown &&
+pip3 install django-filter &&
+pip3 install psycopg2 &&
