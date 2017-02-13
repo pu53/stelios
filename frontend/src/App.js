e25ae45c4a6d0fa47024cb8e50134e5a8e2cdbbd
Kim Borgen <kimatborgen@gmail.com>
2017-02-13 23:26:55 +0100

    create working example of datafetch and rendering

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,10 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+    constructor(props) {
+      super();
+    }
+
   render() {
     return (
       <div className="App">
@@ -13,9 +17,71 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload to kappa
         </p>
+        <List />
       </div>
     );
   }
 }
 
+class List extends Component {
+  constructor(props) {
+    super();
+    this.state = {
+      result: []
+    };
+    this.fetchData = this.fetchData.bind(this);
+  }
+
+  fetchData(event) {
+    event.preventDefault();
+    var request = new Request('http://localhost:8000/users.json', {
+      method: 'GET',
+      headers: {
+              'Accept': 'application/json',
+          },
+    });
+    console.log("yes");
+    fetch(request).then((res) => {
+      return res.json();
+    })
+    .then((res) => {
+      console.log(res);
+      this.setState({ result: res });
+    }).catch((e) => {console.log(e)});
+  }
+
+  render() {
+    console.log(this.state.result)
+    if (this.state.result.length > 0) {
+      return (
+        <div>
+          <button onClick={this.fetchData}>kappa</button>
+          <ul>
+            { this.state.result.map(function(user){
+                return(
+                  <ul>
+                    {Object.keys(user).map(function (key) {
+                      return(
+                        <li>{key}:  {user[key].toString()}</li>
+                      )
+                    })}
+                    <br />
+                  </ul>
+                );
+              })
+            }
+          </ul>
+        </div>
+      );
+    } else {
+      return(
+        <div>
+          <button onClick={this.fetchData}>kappa</button>
+          <li>no data recived, click button ples</li>
+        </div>
+
+      );
+    }
+  }
+}
 export default App;
