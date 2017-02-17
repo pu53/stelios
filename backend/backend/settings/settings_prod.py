bf8b20de40298f8dc914943c04d52c8525c67adf
Kim Borgen <kimatborgen@gmail.com>
2017-02-17 16:28:57 +0100

    add allowed hosts

--- backend/backend/settings/settings_prod.py
+++ backend/backend/settings/settings_prod.py
@@ -4,7 +4,7 @@ from .base import *
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['.stelios.no', '146.185.153.19', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['.stelios.no/', '146.185.153.19/', 'localhost/', '127.0.0.1/', 'stelios.no/', 'api.stelios.no/']
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 STATIC_URL = '/static/'
