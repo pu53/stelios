7fd1ab33bf0bf9a93dee7b1773385085d1ea133c
Kim Borgen <kimatborgen@gmail.com>
2017-02-23 20:38:49 +0100

    redirect to home if url not valid

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import {App, List} from './App';
 import './index.css';
-import { Router, Route, browserHistory, IndexRoute } from 'react-router'
+import { Router, Route, browserHistory, IndexRoute, Redirect} from 'react-router'
 import { Home } from './components/Home'
 import { WikiPage } from './components/WikiPage'
 import '../semantic/dist/semantic.min.css';
@@ -13,6 +13,7 @@ ReactDOM.render((
       <IndexRoute component={Home}/>
       <Route path="/wiki" component={App, WikiPage}/>
       <Route path="/test" component={List} />
+      <Redirect from='*' to='/' />
     </Route>
   </Router>),
   document.getElementById('root')
