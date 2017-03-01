31e75ac2da838134738654840715e6e100702663
Kim Borgen <kimatborgen@gmail.com>
2017-03-01 14:51:14 +0100

    update markdown renderer to remarkable

--- frontend/src/components/SubTopic.jsx
+++ frontend/src/components/SubTopic.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-var ReactMarkdown = require('react-markdown');
+var Remarkable = require('remarkable');
 
 export class SubTopic extends React.Component {
 	constructor(props){
@@ -9,12 +9,17 @@ export class SubTopic extends React.Component {
 
 	render(){
 		var content = this.props.subtopic["content"];
+		var md = new Remarkable('full', {
+			html: true,
+			linkify: true,
+			typographer: true
+	 	});
 		return (
 			<div>
 				<h3>Subtopic: {this.props.subtopic["name"]}</h3>
 				<h4>{this.props.subtopic["description"]}</h4>
 				<br></br>
-				<ReactMarkdown source={content} />
+				<div dangerouslySetInnerHTML={{__html: md.render(content)}} />
 			</div>
 		);
 	}
