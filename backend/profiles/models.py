8eb4d8623bb0cf35e2e3ebd51a6fa72e482017f0
Øystein Krogstie <krogstie.oystein@gmail.com>
2017-03-08 16:03:36 +0100

    properly fix merge conflicts

--- backend/profiles/models.py
+++ backend/profiles/models.py
@@ -2,12 +2,9 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 from django.dispatch import receiver
-<<<<<<< HEAD
 from django.conf import settings
 from rest_framework.authtoken.models import Token
-=======
 import wiki
->>>>>>> a1f97b463965b0b898f79df2b801cb41b7331bb9
 # Create your models here.
 
 # kommentert ut da sudy og year ikke er viktige. kan tas ed senere if need be
@@ -29,13 +26,9 @@ def create_user_profile(sender, instance, created, **kwargs):
 
 @receiver(post_save, sender=User)
 def save_user_profile(sender, instance, **kwargs):
-<<<<<<< HEAD
     instance.profile.save()
 
 @receiver(post_save, sender=settings.AUTH_USER_MODEL)
 def create_auth_token(sender, instance=None, created=False, **kwargs):
     if created:
         Token.objects.create(user=instance)
-=======
-	instance.profile.save()
->>>>>>> a1f97b463965b0b898f79df2b801cb41b7331bb9
