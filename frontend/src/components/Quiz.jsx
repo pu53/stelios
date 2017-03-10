ef1a3640b27ef235eb5227685fc8622a4833e60f
Øystein Krogstie <krogstie.oystein@gmail.com>
2017-03-10 01:56:45 +0100

    add further progres to the quiz

--- frontend/src/components/Quiz.jsx
+++ frontend/src/components/Quiz.jsx
@@ -2,6 +2,8 @@ import React, { Component, PropTypes } from 'react';
 import { Container, Grid, Button } from 'semantic-ui-react';
 import { SearchBar } from './SearchBar';
 
+/*The head element of a quiz. Fetches all the data for all the quiestions included
+ * in the quiz and passes them on to the subcomponents when needed*/
 export class Quiz extends Component {
 	constructor(props) {
 		super(props);
@@ -10,10 +12,10 @@ export class Quiz extends Component {
 			number_of_right:0,
 			number_of_wrong:0,
 			currently_asking:0,
-			subtopics:[]
+			questions:[]
 			};
 			
-		this.setState({subtopics:['hei', 'hallo', 'heisann']})
+		this.setState({questions:{hilsen:'hei', utrop:'ånei!'}})
 	}
 	
 	componentDidMount() {
@@ -39,27 +41,23 @@ export class Quiz extends Component {
 	render() {
 		
 		return(
-			<Grid style={{borderStyle:'solid', marginTop:'40px', padding:'0px'}}>
-				<Grid.Row>
-					<Grid.Column style={{borderStyle:'dashed'}}> {
-						<h1 style={{textAlign:'center'}}>
-						{'This is a quiz in '.concat('testTopic')}
-						</h1>
-					}
-					</Grid.Column>
-				</Grid.Row>
-				<Grid.Row>
-					<Question/>
-				</Grid.Row>
-			</Grid>
+			<Container style={{overflow:'auto', borderStyle:'dashed', position:'relative', minWidth:'100%'}}>
+				<h1 style={{textAlign:'center'}}>
+				{'This is a quiz in '.concat('testTopic')}
+				</h1>
+				<Question/>
+			</Container>
 		);
 	}
 }
 
+/*Representing a single question in a quiz. Manages the answer selection*/
 class Question extends Component {
 	constructor(props) {
 		super(props);
-		this.state={chosen:0};
+		this.state= {
+			chosen:0
+			};
 		
 		this.handleChange = this.handleChange.bind(this);
 	}
@@ -70,27 +68,21 @@ class Question extends Component {
 	
 	render() {
 		return (
-		<Grid style={{borderStyle:'solid'}}>
-			<Grid.Row><Container style={{borderStyle:'solid'}}>
-				Here comes the current question beeing asked, Lorem ipsum dolor 
-				sit amet, consectetur adipiscing elit. Suspendisse id lobortis 
-				tortor. Pellentesque sapien tortor, semper vehicula ipsum sed, 
-				vehicula ullamcorper nunc. Nam in ante eget velit iaculis sodales.
-				Sed tincidunt arcu tellus, pharetra eleifend mauris tincidunt non?
-			</Container></Grid.Row>
-			<Grid.Row centered style={{borderStyle:'dotted'}}><Container>
-				<Grid.Row style={{borderStyle:'dashed', width:'100%',margin:'auto'}}>
-					<Answer opNr="1"/>
-				</Grid.Row>
-				<Grid.Row><Answer opNr="2"/></Grid.Row>
-				<Grid.Row><Answer opNr="3"/></Grid.Row>
-				<Grid.Row><Answer opNr="4"/></Grid.Row>
-			</Container></Grid.Row>
-		</Grid>
+			<div style={{marginLeft:'15%', marginRight:'15%', width:'70%', borderStyle:'dotted', overflow:'hidden', position:'relative'}}>
+				<div text style={{fontSize:'20', overflow:'hidden', padding:'5px'}}>
+					How and why the great badger of doom ended the mayas has long been a hotly debated topic
+					in south american ancient history. What, however, is today the most recognised theory?
+				</div>
+				<Answer opNr={1}/>
+				<Answer opNr={2}/>
+				<Answer opNr={3}/>
+				<Answer opNr={4}/>
+			</div>
 		)
 	}
 }
-
+/*A simle custom button for presenting an answer option. What option is toggled is
+ * monitored from the Question owning the answer*/
 class Answer extends Component {
 	constructor(props) {
 		super(props);
@@ -107,10 +99,10 @@ class Answer extends Component {
 	}
 	
 	render() {
-		
-		
 		return (
-		<Button onClick={this.handleChange}>Option {this.state.idNum}</Button>
+		<div style={{cursor:'pointer', textAlign:'center'}} onClick={this.handleChange}>
+			Option {this.state.idNum}
+		</div>
 		)
 	}
 }
