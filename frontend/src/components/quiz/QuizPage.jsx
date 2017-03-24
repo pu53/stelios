d1b884b0122734bb16b486ca007a1a3842239cb0
Øystein Krogstie <krogstie.oystein@gmail.com>
2017-03-24 15:39:06 +0100

    add misc code cleanup and prepare for merge

--- frontend/src/components/quiz/QuizPage.jsx
+++ frontend/src/components/quiz/QuizPage.jsx
@@ -1,9 +1,7 @@
 import React, { Component} from 'react';
-import { Container, Grid, Button, Segment} from 'semantic-ui-react';
-import { SearchBar } from '../SearchBar';
+import { Grid, Button } from 'semantic-ui-react';
 import { Quiz } from './Quiz';
 import { QuizList } from './QuizList.jsx';
-import { getData } from '../../helpers.jsx';
 import '../../styles/quiz_page.css';
 import './quiz_page.css'
 import { Template } from './Template'
@@ -13,7 +11,6 @@ export class QuizPage extends Component {
 	constructor(props) {
 		super(props);
 		this.state = {
-			inQuiz:true,
 			quizData:[],
 			quizState: "inQuiz", //state variable to select state of quiz. none, inQuiz and newQuiz are avalible options.
 			url_suffix:"quiz/data/",
@@ -23,29 +20,30 @@ export class QuizPage extends Component {
 			neg: false
 		};
 		this.fetchData = this.fetchData.bind(this)
-		this.done_loading = false
 	}
 	
 	componentWillMount() {
 		this.fetchData()
 	}
-  newQuizData = (quiz_data) => {
-    this.setState({
-    })
-      quiz_data
-  }
+	 newQuizData = (quiz_data) => {
+		this.setState({
+			quiz_data
+		})
+	}
 	fetchData() {
-		var url = this.state.url_suffix
+		var suffix = this.state.url_suffix
 		var link = '';
+		//Constructing request link, by choosing enviroment
 		if (!process.env.NODE_ENV || process.env.NODE_ENV === 'development') {
-			link = 'http://localhost:8000/'+ url;
+			link = 'http://localhost:8000/';
 		} else {
-			link = 'http://api.stelios.no/'+ url;
+			link = 'http://api.stelios.no/';
 		}
 		
-		link += this.props.params.quizId;
+		//Adding suffix and parameter to the link path, completing it
+		link += suffix + this.props.params.quizId;
 		
-		//generated request
+		//Generate request
 		var request = new Request(link, {
 			method: 'GET',
 			headers: {
@@ -53,66 +51,21 @@ export class QuizPage extends Component {
 			},
 		});
 		
+		//Fetch data
 		fetch(request).then((res) => {
-			console.log("Status: "+ res.status)
+			//console.log("Status: "+ res.status)
 			return res.json();
 		})
 		.then((res) => {
-			console.log("found data: " + res);
+			//console.log("found data: " + res);
 			this.setState({
 				quizData:res
 			}, this.finishLoad())
 		}).catch((e) => {
 			console.log(e);
 		});
-	
-		
-		
-		/*
-		const question1 = {
-		id:4,
-		text:"How and why the great badger of doom ended "+
-			"the mayas has long been a hotly debated topic "+
-			"in south american ancient history. What, however, "+
-			"is today the most recognised theory?",
-		alternatives:[
-			{id:1, choice_text:"It ate them"},
-			{id:2, choice_text:"It scared them to death"},
-			{id:3, choice_text:"The adverse effect the badger had on local fauna"}
-			],
-		subtopic:'badgers',
-		};
-		
-		const question2 = {
-		id:7,
-		text:"What is considered the most influential paper on tea and crackers?",
-		alternatives:[
-			{id:1, choice_text:"Objectivity in a subjective science, on the importance of peer review when doing taste tests"},
-			{id:2, choice_text:"Air humidity and cracker elasticity"},
-			{id:3, choice_text:"An unhealthy orthodoxy-on how black tea has been displaced by fruit tea"}
-			],
-		subtopic:'foodstuffs',
-		};
-		
-		const question3 = {
-		id:8,
-		text:"Which data structure benefits greatly when implementations do so-called \"Robin Hooding\"?",
-		alternatives:[
-			{id:1, choice_text:"Priority Stack"},
-			{id:2, choice_text:"Hash Map"},
-			{id:3, choice_text:"Priority Queue"},
-			{id:5, choice_text:"Red-Black Tree"}
-			],
-		subtopic:'Algorithms',
-		};
-		var all_questions = [question1, question2, question3];
-		var data = {
-			questions:all_questions,
-			title:"This is a quiz from data passed through props"
-		};
-		this.setState({quiz_data:data});
-		*/
 	}
+	
 	onNewClick = (e) => {
 		e.preventDefault()
 		this.setState({
@@ -140,7 +93,7 @@ export class QuizPage extends Component {
 	}
 	
 	render() {
-		if(this.state.quizState === "inQuiz" && this.done_loading  && this.state.quizData.questions !== undefined) {
+		if(this.state.quizState === "inQuiz" && this.state.quizData.questions !== undefined) {
 			console.log("in render, the data in state is: ");
 		
 			return(
@@ -156,25 +109,24 @@ export class QuizPage extends Component {
 				</Grid>
 			);
 		}
-    else if (this.state.quizState === "newQuiz") {
-      return (
-        <Grid>
+		else if (this.state.quizState === "newQuiz") {
+			return (
+				<Grid>
 					<Grid.Column width={16}>
 						<CustomMessage onChangeMessage={this.onChangeMessage} status={-1} message={this.state.message} neg={true} />
 					</Grid.Column>
-          <Grid.Column width={16}>
-            <Template
-              new={true}
-              newQuizData={this.newQuizData}
-              quizData={this.state.quizData}
-							onChangeMessage={this.onChangeMessage}
-							changeQuizState={this.changeQuizState}
-              />
-          </Grid.Column>
-
-        </Grid>
-      )
-    }
+				<Grid.Column width={16}>
+					<Template
+						new={true}
+						newQuizData={this.newQuizData}
+						quizData={this.state.quizData}
+						onChangeMessage={this.onChangeMessage}
+						changeQuizState={this.changeQuizState}
+						/>
+					</Grid.Column>
+				</Grid>
+			)
+		}
 		else {
 			return(
 				<Grid>
