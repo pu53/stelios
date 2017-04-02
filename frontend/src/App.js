b38dbd39a010745b9a785a934a35ef6c168c3227
kim borgen <kimatborgen@gmail.com>
2017-04-02 20:01:04 +0200

    Fix login_text when new user is visiting and get logo from stelios.no

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -50,12 +50,12 @@ export class App extends Component {
     }
 
   render() {
-    var login_text = this.state.token === "null" ? "login" : "logout"
+    var login_text = this.state.token === "null" || this.state.token === null ? "login" : "logout"
     return (
       <div className="App" style={{width:'100%'}}>
         <Segment raised style={{"color":"#FFFFFF","background-color":"#3F51B5","padding":"0 0 0 0"}}>
           <div style={{"display":"flex", "align-items":"center","margin-top":"px", "margin-bottom":"25px"}}>
-            <Image inverted style={{"margin-left":"20px", "margin-top":"23px", "margin-right":"20px"}} src={process.env.PUBLIC_URL + "logo.png"} width="50px" height="50px" shape="circular"/>
+            <Image inverted style={{"margin-left":"20px", "margin-top":"23px", "margin-right":"20px"}} src="stelios.no/logo.png" width="50px" height="50px" shape="circular"/>
             <h1>Stelios</h1>
           </div>
           <div style={{"width":"100%", "background-color": "#303F9F"}}>
