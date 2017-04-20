4fc1a990c47b32c67a826c3db38609fc4e8a6f46
Andreas Berger <Andreas.Berger.ab@gmail.com>
2017-04-20 15:32:22 +0200

    remove redunant code in userpage

--- frontend/src/components/user/WelcomeMessage.jsx
+++ frontend/src/components/user/WelcomeMessage.jsx
@@ -4,62 +4,16 @@ import { Container, Header } from 'semantic-ui-react'
 export class WelcomeMessage extends React.Component {
     constructor(props) {
 		super(props);
-		this.state = ({
-			data: {},
-			value: ''
-		});
 	}
 
-    fetchData(){
-        var link = '';
-        var url = 'users/data/' + localStorage.getItem('stelios_current_user') + '/?format=json';
-        if (!process.env.NODE_ENV || process.env.NODE_ENV === 'development') {
-          link = 'http://localhost:8000/'+ url;
-        } else {
-          link = 'http://api.stelios.no/'+ url;
-        }
-        //generated request
-        var request = new Request(link, {
-          method: 'GET',
-          headers: {
-            'Accept': 'application/json',
-          },
-        });
-
-        fetch(request).then((res) => {
-          return res.json();
-        })
-        .then((res) => {
-          this.setState({data:res});
-        }).catch((e) => {
-          console.log(e);
-        });
-    }
-
-    componentWillMount(){
-        this.fetchData();
-    }
-
     render() {
-        if(Object.keys(this.state.data).length > 0){
-            return (
-                <div>
-                    <Container fluid>
-                       <Header as='h1'>{'Welcome '+this.state.data.username+'!'}</Header>
-                       <br/>
-                    </Container>
-               </div>
-
-            );
-        } else {
-            return(
-                <div>
-                    <Container fluid>
-                        <Header as='h1'>Welcome to the STELIOS User Page!</Header>
-                        <br/>
-                    </Container>
-                </div>
-            )
-        }
+        return (
+            <div>
+                <Container fluid>
+                   <Header as='h1'>{'Welcome '+ this.props.username +'!'}</Header>
+                   <br/>
+                </Container>
+           </div>
+        );
     }
 }
