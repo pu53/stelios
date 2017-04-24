eb69e321729a5175a9ecd9dce855705905379358
kim borgen <kimatborgen@gmail.com>
2017-04-24 18:56:57 +0200

    hide edit/create/new buttons if user is not professor

--- frontend/src/components/wiki/ButtonGroup.jsx
+++ frontend/src/components/wiki/ButtonGroup.jsx
@@ -80,9 +80,9 @@ export class ButtonGroup extends React.Component {
         return (
           <Button.Group basic float="right">
             {this.SubscribeToSubject()}
-            {this.props.edit ? <Button content="Edit" onClick={this.props.edit} /> : null }
-            {this.props.new ? <Button content="New" onClick={this.props.new} /> : null }
-            {this.props.delete ? <Button basic negative content="Delete" onClick={this.props.delete} /> : null }
+            {this.props.edit && this.props.steliosUserProfessor !== "false" ? <Button content="Edit" onClick={this.props.edit} /> : null }
+            {this.props.new && this.props.steliosUserProfessor !== "false" ? <Button content="New" onClick={this.props.new} /> : null }
+            {this.props.delete && this.props.steliosUserProfessor !== "false" ? <Button basic negative content="Delete" onClick={this.props.delete} /> : null }
           </Button.Group>
         );
       }
