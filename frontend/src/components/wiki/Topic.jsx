87d41604a7ca1177e804202085ba08a10f5539d9
kim borgen <kimatborgen@gmail.com>
2017-04-27 04:06:55 +0200

    change more of localStorage "null" -> object null

--- frontend/src/components/wiki/Topic.jsx
+++ frontend/src/components/wiki/Topic.jsx
@@ -268,7 +268,7 @@ export class Topic extends React.Component {
 				name={this.state.name}
 				description={this.state.description}
 			/>
-		{ this.props.steliosToken !== "null" && this.state.activeTopicId !== -1 && this.props.steliosUserProfessor !== "false" ?
+		{ this.props.steliosToken !== null && this.state.activeTopicId !== -1 && this.props.steliosUserProfessor !== "false" ?
 				<div>
 					<Button style={{"marginRight":"4%"}} basic floated="right" content="Create a new subtopic" onClick={this.onNewSubTopic}/>
 					<br />
@@ -294,7 +294,7 @@ export class Topic extends React.Component {
 
 			</Element>
 			}
-			{ this.props.steliosToken !== "null" && this.state.activeTopicId !== -1 && this.props.steliosUserProfessor !== "false" && this.state.subtopics.length > 0  ?
+			{ this.props.steliosToken !== null && this.state.activeTopicId !== -1 && this.props.steliosUserProfessor !== "false" && this.state.subtopics.length > 0  ?
 			<div>
 				<Button style={{"marginRight":"4%"}} basic floated="right" content="Create a new subtopic" onClick={this.onNewSubTopic}/>
 				<br />
