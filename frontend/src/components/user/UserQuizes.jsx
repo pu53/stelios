0a6bac50182d0d70ba89181decbb916c2262db6f
Andreas Berger <Andreas.Berger.ab@gmail.com>
2017-04-27 05:49:06 +0200

    fix minor graphics bug

--- frontend/src/components/user/UserQuizes.jsx
+++ frontend/src/components/user/UserQuizes.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Link } from 'react-router';
-import { Menu, Label, Header, Icon } from 'semantic-ui-react'
+import { Menu, Label, Header, Icon, Segment } from 'semantic-ui-react'
 
 export class UserQuizes extends React.Component {
     constructor(props) {
@@ -13,21 +13,23 @@ export class UserQuizes extends React.Component {
             <div>
                 <br />
                 <Header size='large'>Quizes</Header>
-                <Menu fluid vertical>
-                    {this.props.quizes.map(function(quiz, index){
-                    return(
-                        <div>
-                        {localStorage.getItem('stelios_current_user_professor')==='true'?
-                        //If the user is a professor the quiz list will have links to statistics
-                        <Link to={'/quiz/'+quiz.id} key={index}><Menu.Item ><Icon name='tasks' /> <Label color='teal'><Link to={'/stat/quiz/'+quiz.id}>Statistics</Link></Label>{ quiz.title }</Menu.Item></Link>
-                        :
-                        //otherwise the list will only show links to do the quizes
-                        <Link to={'/quiz/'+quiz.id} key={index}><Menu.Item ><Icon name='tasks' /> <Label color='teal'>{quiz.deadline}</Label>{ quiz.title }</Menu.Item></Link>
-                        }
-                        </div>
-                    )
-                    })}
-                </Menu>
+                <Segment style={{'padding':'0'}}>
+                    <Menu fluid vertical>
+                        {this.props.quizes.map(function(quiz, index){
+                        return(
+                            <div>
+                            {localStorage.getItem('stelios_current_user_professor')==='true'?
+                            //If the user is a professor the quiz list will have links to statistics
+                            <Link to={'/quiz/'+quiz.id} key={index}><Menu.Item ><Icon name='tasks' /> <Label color='teal'><Link to={'/stat/quiz/'+quiz.id}>Statistics</Link></Label>{ quiz.title }</Menu.Item></Link>
+                            :
+                            //otherwise the list will only show links to do the quizes
+                            <Link to={'/quiz/'+quiz.id} key={index}><Menu.Item ><Icon name='tasks' /> <Label color='teal'>{quiz.deadline}</Label>{ quiz.title }</Menu.Item></Link>
+                            }
+                            </div>
+                        )
+                        })}
+                    </Menu>
+                </Segment>
             </div>
         );
     }
