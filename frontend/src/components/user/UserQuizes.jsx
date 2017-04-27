d0a19be034db9376566c46830c5306adb82c0186
Andreas Berger <Andreas.Berger.ab@gmail.com>
2017-04-27 05:33:55 +0200

    update quiz list to link to statistics

--- frontend/src/components/user/UserQuizes.jsx
+++ frontend/src/components/user/UserQuizes.jsx
@@ -1,74 +1,34 @@
 import React from 'react'
+import { Link } from 'react-router';
+import { Menu, Label, Header, Icon } from 'semantic-ui-react'
 
-import { Menu, Label, Header, Icon, Grid, Segment } from 'semantic-ui-react'
-import { Link } from 'react-router'
-
-/* Creates a list of links to a users quizes */
 export class UserQuizes extends React.Component {
-	constructor(props) {
-		super(props);
-	}
+    constructor(props) {
+        super(props);
+    }
 
-	render() {
-		/*Checks if the current user is a professor, and shows/hides the stats
-		 * button accordingly */
-		return (
-			<div>
-			<br />
-			<Header size='large'>Quizes</Header>
-				{localStorage.getItem('stelios_current_user_professor')==='true'?
-					<Segment style={{"padding":"0 0 0 0"}}>
-					<Menu fluid vertical>
-					{this.props.quizes.map(function(quiz, index){
-						return <div key={index}>
-							<Grid><Grid.Row>
-							{/*First comes the quiz link*/}
-							<Grid.Column width={12}>
-								<Link to={'/quiz/'+quiz.id} key={index}>
-									<Menu.Item>
-										<Icon name='tasks' />
-										<Label color='teal'>
-											{quiz.deadline}
-										</Label>{ quiz.title }
-									</Menu.Item>
-								</Link>
-							</Grid.Column>
-							{/*Then the statistics link*/}
-							<Grid.Column width={4}>
-								<Link to={"/stat/quiz/"+quiz.id}>
-									<div style={{
-										'height':'100%', 
-										'borderLeft':'solid',
-										'borderWidth':'2px',
-										'borderColor':'#666666',
-										'display':'flex', 
-										'alignItems':'center',
-										'color':'black', 
-										'justifyContent':'center'}}>
-										Statistics
-									</div>
-								</Link>
-							</Grid.Column>
-							</Grid.Row></Grid>
-						</div>;
-						})}
-				</Menu>
-				</Segment>
-				:<Segment style={{"padding":"0 0 0 0"}}><Menu fluid vertical>
-					{this.props.quizes.map(function(quiz, index){
-						return 
-						<Link to={'/quiz/'+quiz.id} key={index}> 
-							<Menu.Item> <Icon name='tasks' /> 
-								<Label color='teal'>
-									{quiz.deadline}
-								</Label>{ quiz.title }
-							</Menu.Item>
-						</Link>;
-					})}
-				</Menu>
-				</Segment>
-				}
-			</div>
-		);
-	}
+    /* Creates a list of links to a users quizes */
+    render() {
+        return (
+            <div>
+                <br />
+                <Header size='large'>Quizes</Header>
+                <Menu fluid vertical>
+                    {this.props.quizes.map(function(quiz, index){
+                    return(
+                        <div>
+                        {localStorage.getItem('stelios_current_user_professor')==='true'?
+                        //If the user is a professor the quiz list will have links to statistics
+                        <Link to={'/quiz/'+quiz.id} key={index}><Menu.Item ><Icon name='tasks' /> <Label color='teal'><Link to={'/stat/quiz/'+quiz.id}>Statistics</Link></Label>{ quiz.title }</Menu.Item></Link>
+                        :
+                        //otherwise the list will only show links to do the quizes
+                        <Link to={'/quiz/'+quiz.id} key={index}><Menu.Item ><Icon name='tasks' /> <Label color='teal'>{quiz.deadline}</Label>{ quiz.title }</Menu.Item></Link>
+                        }
+                        </div>
+                    )
+                    })}
+                </Menu>
+            </div>
+        );
+    }
 }
